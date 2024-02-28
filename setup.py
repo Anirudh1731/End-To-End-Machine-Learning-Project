e3cdd9773334228feba72badfa99ba42a976f41d
Anirudh1731 <anirudhsanthosh1731@gmail.com>
2024-02-28 23:36:35 +0530

    requirements and setup added

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages,setup
+
+from typing import List
+
+
+Hypen_e_dot='-e.'
+
+# going through each items in the requirements.txt to get the items to be installed 
+def get_requirements(filepath: str)-> List[str]:
+
+    requirements=[]
+
+    with open(filepath) as file_obj:
+        requirements=file_obj.readlines()
+
+        requirements=[req.replace("\n","") for req in requirements ]
+
+        if Hypen_e_dot in requirements:
+            requirements.remove(Hypen_e_dot)
+
+    return requirements
+
+# this will create packages of my project like any package in python , this will get automatically get triggered due to -e. in requiremets.txt 
+setup(
+    name='End-To-End-Machine-Learning-Project',
+    version='0.0.1',
+    author='Anirudh1731',
+    author_mail='anirudhsanthosh1729@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
